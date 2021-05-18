d1dc46454a254adb4c7be207cc5b69d3f7ab983a
Euihyun Lee <euihyun.dev@gmail.com>
2021-05-18 16:58:25 +0900

    Add json dumps hook and change query execution method (do escaping to strings)

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import pymysql
 from sanic import Sanic
 from sanic.response import json, text
+from functools import partial
 
 import utils
 import config
@@ -15,14 +16,15 @@ db = pymysql.connect(
     charset='utf8'
 )
 cursor = db.cursor(pymysql.cursors.DictCursor)
+json = partial(json, dumps=utils.mysql_json_dumps())
 
 
 # CRUD
 # READ API
 @app.route('/user/<user_id>', methods=["GET"])
 async def get_user_by_id(request, user_id):
-    query = f"SELECT * FROM user WHERE user_id = {user_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM user WHERE user_id = %s;"
+    cursor.execute(query, user_id)
     result = cursor.fetchall()
     assert len(result) <= 1, f"Duplicated user ID: {user_id}"
     if len(result) == 0:
@@ -35,33 +37,34 @@ async def get_user_by_id(request, user_id):
 async def get_star_by_user(request, user_id):
     if "weekday" in request.args:
         weekday = request.raw_args["weekday"]
-        query = f"SELECT * FROM star WHERE user_id = {user_id} AND weekday = {weekday};"
+        query = "SELECT * FROM star WHERE user_id = %s AND weekday = %s;"
+        cursor.execute(query, user_id, weekday)
     else:
-        query = f"SELECT * FROM star WHERE user_id = {user_id};"
-    cursor.execute(query)
+        query = "SELECT * FROM star WHERE user_id = %s;"
+        cursor.execute(query, user_id)
     result = cursor.fetchall()
     return json(result)
 
 
 @app.route('/user/<user_id>/toon/<toon_id>/star', methods=["GET"])
 async def get_star_by_user_toon(request, user_id, toon_id):
-    query = f"SELECT * FROM star WHERE user_id = {user_id} AND toon_id = {toon_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM star WHERE user_id = %s AND toon_id = %s;"
+    cursor.execute(query, user_id, toon_id)
     result = cursor.fetchall()
     return json(result)
 
 
 @app.route('/user/<user_id>/history', methods=["GET"])
 async def get_history_by_user(request, user_id):
-    query = f"SELECT * FROM view_history WHERE user_id = {user_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM view_history WHERE user_id = %s;"
+    cursor.execute(query, user_id)
     result = cursor.fetchall()
     return json(result)
 
 
 @app.route('/toon', methods=["GET"])
 async def get_toon(request):
-    query = f"SELECT * FROM toon;"
+    query = "SELECT * FROM toon;"
     cursor.execute(query)
     result = cursor.fetchall()
     return json(result)
@@ -69,8 +72,8 @@ async def get_toon(request):
 
 @app.route('/toon/<toon_id>', methods=["GET"])
 async def get_toon_by_id(request, toon_id):
-    query = f"SELECT * FROM toon WHERE toon_id = {toon_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM toon WHERE toon_id = %s;"
+    cursor.execute(query, toon_id)
     result = cursor.fetchall()
     assert len(result) <= 1, f"Duplicated toon ID: {toon_id}"
     if len(result) == 0:
@@ -81,16 +84,16 @@ async def get_toon_by_id(request, toon_id):
 
 @app.route('/toon/<toon_id>/episode', methods=["GET"])
 async def get_episode_by_toon(request, toon_id):
-    query = f"SELECT * FROM episode WHERE toon_id = {toon_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM episode WHERE toon_id = %s;"
+    cursor.execute(query, toon_id)
     result = cursor.fetchall()
     return json(result)
 
 
 @app.route('/star/<star_id>', methods=["GET"])
 async def get_star_by_id(request, star_id):
-    query = f"SELECT * FROM star WHERE star_id = {star_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM star WHERE star_id = %s;"
+    cursor.execute(query, star_id)
     result = cursor.fetchall()
     assert len(result) <= 1, f"Duplicated star ID: {star_id}"
     if len(result) == 0:
@@ -101,8 +104,8 @@ async def get_star_by_id(request, star_id):
 
 @app.route('/episode/<episode_id>', methods=["GET"])
 async def get_episode_by_id(request, episode_id):
-    query = f"SELECT * FROM episode WHERE episode_id = {episode_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM episode WHERE episode_id = %s;"
+    cursor.execute(query, episode_id)
     result = cursor.fetchall()
     assert len(result) <= 1, f"Duplicated episode ID: {episode_id}"
     if len(result) == 0:
@@ -113,8 +116,8 @@ async def get_episode_by_id(request, episode_id):
 
 @app.route('/episode/<episode_id>/history', methods=["GET"])
 async def get_history_by_episode(request, episode_id):
-    query = f"SELECT * FROM view_history WHERE episode_id = {episode_id};"
-    cursor.execute(query)
+    query = "SELECT * FROM view_history WHERE episode_id = %s;"
+    cursor.execute(query, episode_id)
     result = cursor.fetchall()
     return json(result)
 
